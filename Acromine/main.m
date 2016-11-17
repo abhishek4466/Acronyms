7324ab759aea99a607d2418d073206ec7b5e061f
Abhishek Tyagi <abhishektyagi@softwares-imac.home>
2016-11-17 13:00:15 -0500

    Initial Commit

---
+++ Acromine/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Acromine
+//
+//  Created by Abhishek Tyagi on 11/17/16.
+//  Copyright © 2016 Department of Education. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
